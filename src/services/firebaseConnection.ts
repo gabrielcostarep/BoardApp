53b933a3f0c4ffbd8030682e09400f89c85d6bf7
Gabriel Costa <119979526+gabrielcostarep@users.noreply.github.com>
2023-04-29 00:13:33 -0300

    Update firebaseConnection.ts

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADY3paDdcoWwypnnWS1KXhKj2TZuK9kbg",
-  authDomain: "boardapp-4068e.firebaseapp.com",
-  projectId: "boardapp-4068e",
-  storageBucket: "boardapp-4068e.appspot.com",
-  messagingSenderId: "1052464381365",
-  appId: "1:1052464381365:web:fdb2ad1a2be7b98a5c2dfc",
-  measurementId: "G-NHG588CZ9W"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
